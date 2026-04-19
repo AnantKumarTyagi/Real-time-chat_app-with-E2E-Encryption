0fdd42c258ea089201a13858e679363a506afc45
unknown <ananttyagi799@gmail.com>
2026-04-19 09:39:52 +0530

    updated useAuthstore for production

--- Frontend/src/store/useAuthStore.js
+++ Frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 import { generateKeyPair, exportPublicKey, wrapKeyWithPin, unwrapKeyWithPin } from "../lib/crypto.js";
-import { savePrivateKey, getPrivateKey, clearPrivateKey } from "../lib/KeyStorage.js";
+import { savePrivateKey, getPrivateKey, clearPrivateKey } from "../lib/keyStorage.js";
 const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
 
 export const useAuthStore = create((set, get) => ({
